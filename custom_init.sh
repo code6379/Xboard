eb6bea103effcd26e4d3822cd644bf6d7fa71c59
coder.peng <1047871481@qq.com>
2026-07-21 17:46:52 +0800

    refactor(docker): 更新 Docker 配置和初始化脚本
    - 将 Redis 数据卷路径从 ./data/rdb 更改为 ./data/redis
    - 将数据库数据卷路径从 ./data/db 更改为 ./data/database
    - 移除资源配置文件、Horizon 启用选项和 Docker 标识环境变量
    - 删除自定义网络配置并使用默认网络
    - 更新初始化脚本以支持 Git 克隆和模板复制
    - 修改 Composer 安装命令使用新的 Docker Compose 语法
    - 简化 Xboard 安装流程并移除自动启动命令

--- custom_init.sh
+++ custom_init.sh
@@ -1,11 +1,19 @@
 #!/usr/bin/env sh
 set -eu
 
+XBOARD_REPO="https://github.com/code6379/Xboard.git"
+XBOARD_BRANCH="master"
+COMPOSE_FILE="compose.custom.yaml"
+
+
+echo "Cloning Xboard source..."
+git clone --depth 1 --branch "$XBOARD_BRANCH" --recurse-submodules "$XBOARD_REPO" www
+
+echo "Using Compose template: www/$COMPOSE_FILE"
+cp "www/$COMPOSE_FILE" "$COMPOSE_FILE"
+
 echo "Installing Composer dependencies..."
-docker-compose -f compose.custom.yaml run --rm --entrypoint sh xboard -c 'cd /www && composer install --no-dev --optimize-autoloader'
+docker compose run --rm --entrypoint sh xboard -c 'cd /www && composer install --no-dev --optimize-autoloader -vvv'
 
 echo "Running Xboard installer..."
-docker-compose -f compose.custom.yaml run -it --rm xboard php artisan xboard:install
-
-echo "Starting Xboard..."
-docker-compose -f compose.custom.yaml up -d
+docker compose run -it --rm xboard php artisan xboard:install
